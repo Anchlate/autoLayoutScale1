6b6a77817aaaef2eea5cfbd79be269492097d4ac
Qianrun <Qianrun@QianrundeMini.lan>
2017-05-09 09:45:26 +0800

    first

---
+++ autoLayoutScall/UIButton+ANFont.h
@@ -0,0 +1,25 @@
+//
+//  UIButton+ANFont.h
+//  UIFontLayout
+//
+//  Created by Qianrun on 16/8/5.
+//  Copyright © 2016年 qianrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+/**
+ *  Button
+ */
+
+@interface UIButton (ANFont)
+
+@end
+
+/**
+ *  Label
+ */
+@interface UILabel (myFont)
+
+@end
--- autoLayoutScall/ViewController.m
+++ autoLayoutScall/ViewController.m
@@ -7,8 +7,32 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+
+/**
+ *  you will get the screen width when you use the SCREEN_WIDTH
+ *
+ */
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+
+/**
+ *  you will get the screen height when you user the SCREEN_HEIGHT
+ *
+ */
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+
+#define kScalX ((SCREEN_WIDTH != 375) ? (SCREEN_WIDTH / 375) : 1.0)
+#define kScalY ((SCREEN_WIDTH != 375) ? (SCREEN_HEIGHT / 667) : 1.0)
+
+
+#define WEAKSELF(weakSelf) __weak __typeof(&*self)weakSelf = self
+
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *headView;
+@property (strong, nonatomic) IBOutlet UILabel *ALabel;
 
 @end
 
@@ -16,12 +40,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"......xscal:%f", kScalX);
+    NSLog(@"......yscal:%f", kScalY);
+    NSLog(@"..........alabel:%@", self.ALabel.font);
+    NSLog(@"......albel frame:%@", NSStringFromCGRect(self.ALabel.frame));
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIView *)headView {
+    
+    
+    if (!_headView) {
+        _headView = [[UIView alloc]init];
+        _headView.backgroundColor= [UIColor blueColor];
+    }
+    return _headView;
 }
 
 @end
